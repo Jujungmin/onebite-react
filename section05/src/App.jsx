23d9848d71dd7f46d9d8b823eef97965a524bdd7
jungmin <43zuhzuh@gmail.com>
2026-06-23 18:05:40 +0900

    React.js입문

---
+++ section05/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>안녕 리액트!</main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
