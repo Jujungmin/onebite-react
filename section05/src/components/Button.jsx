1dc8db4f3630938a1eedb5e46625ce88c2c665ce
jungmin <43zuhzuh@gmail.com>
2026-06-25 16:04:27 +0900

    React.js입문-이벤트처리하기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,11 +1,13 @@
-const Button = ({children, text, color = 'black', ...rest}) => {
-  // console.log(props);
-  // 자식 컴포넌트에게 프롭스로 전달해주면 매개변수에 전달되게 된다.
-  console.log(rest)
+const Button = ({children, text, color = 'black'}) => {
+  // 이벤트 핸들링: 이벤트가 발생했을 때 그것을 처리하는 것
+  const onClickButton = (e) => {
+    console.log(e); // SyntheticBaseEvent 합성이벤트: 모든 웹 브라우저의 이벤트객체를 하나로 통일한 형태
+  }
   return (
-    <button style={{color: color}}>
+    <button
+      onClick={onClickButton}
+      style={{color: color}}>
       {text} - {color.toUpperCase()}
-      {/* undefined로 설정되어 오류가 난다. */}
       {children}
     </button>
   )
