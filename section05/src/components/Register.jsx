0e62a23acb36355b89e10a753d5aad86f272f061
jungmin <43zuhzuh@gmail.com>
2026-06-26 14:52:56 +0900

    React.js입문-State와사용자입력관리하기2

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -4,54 +4,96 @@ import { useState } from "react";
 // 2. 생년월일
 // 3. 국적
 // 4. 자기소개
+
+// 객체로 만들어서 하나의 스테이트 묶음
 const Register = () => {
-  const [name, setName] = useState('이름');
-  const [birth, setBirth] = useState('');
-  const [country, setCountry] = useState('');
-  const [bio, setBio] = useState('');
+
+  const [num, setNum] = useState(0);
+  console.log(num);
+  console.log(typeof setNum);
+
+  const [input, setInput] = useState({
+    name: '',
+    birth: '',
+    country: '',
+    bio: '',
+  });
+
+  // 여러 개 비슷하게 동작하는 이벤트 하나로 묶음 : 통합 이벤트 핸들러
+  const onChange = (e) => {
+    // console.log(e.target.name, e.target.value);
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+      // 각 input의 name값 호출
+    })
+  }
+  // setInput상태변화 함수 호출
+  // 스프레드 연산자로 기존 input값 나열
   
-  const onChangeName = (e) => {
-    setName(e.target.value); // name 스테이트에 최종적으로 전달완료
-  };
-  const onChangeBirth = (e) => {
-    setBirth(e.target.value);
-  };
-  const onChangeCountry = (e) => {
-    setCountry(e.target.value);
-  };
-  const onChangeBio = (e) => {
-    setBio(e.target.value);
-  };
+  // const onChangeName = (e) => {
+  //   setInput({
+  //     ...input, // 스프레드기법(모두 가져오기)
+  //     name: e.target.value // 변경하고자하는 자식(properties) 값
+  //   });
+  // };
+  // const onChangeBirth = (e) => {
+  //   setInput({
+  //     ...input,
+  //     birth: e.target.value
+  //   });
+  // };
+  // const onChangeCountry = (e) => {
+  //   setInput({
+  //     ...input,
+  //     country: e.target.value
+  //   });
+  // };
+  // const onChangeBio = (e) => {
+  //   setInput(
+  //     {
+  //     ...input,
+  //     bio: e.target.value
+  //   }
+  //   );
+  // };
   
   return (
     <div>
       <div>
         <input 
-          value={name}
+          name="name"
+          value={input.name}
           placeholder={'이름'}
-          onChange={onChangeName}
+          onChange={onChange}
           />
       </div>
       <div>
         <input 
+          name="birth"
           type="date"
-          value={birth}
-          onChange={onChangeBirth}
+          value={input.birth}
+          onChange={onChange}
         />
       </div>
       <div>
-        <select value={country} onChange={onChangeCountry}>
+        <select 
+          name="country"
+          value={input.country}
+          onChange={onChange}
+        >
           <option value=""></option>
           <option value="kr">한국</option>
           <option value="us">미국</option>
           <option value="uk">영국</option>
         </select>
       </div>
-
       <div>
-        <textarea value={bio} onChange={onChangeBio}>
-          {bio}
-        </textarea>
+        <textarea
+          name="bio" 
+          value={input.bio}
+          onChange={onChange}
+        />
       </div>
     </div>
   )
