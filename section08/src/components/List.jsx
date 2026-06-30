c8fba6fda1db7407d75ecc4bc6fae51874be8b94
jungmin <43zuhzuh@gmail.com>
2026-06-30 18:21:02 +0900

    프로젝트2.투두리스트-Read:투두리스트렌더링하기

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,15 +1,35 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState('');
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  const getFilterData = () => {
+    if(search === '') return todos; // 빈 값일 때는 todos뿌리기
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()
+    ));
+  }
+
+  const fitleredTodos = getFilterData();
+
   return (
     <div className="List">
       <h4>Todo List 🌱</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch} 
+        placeholder="검색어를 입력하세요" 
+      />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {fitleredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} /> // ...todo의 기능 => TodoItem의 props를 가져오기 위해
+        })}
       </div>
     </div>
   )
