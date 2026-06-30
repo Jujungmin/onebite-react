c0a20ac63ee910ceda04439b4aafb39655edc14c
jungmin <43zuhzuh@gmail.com>
2026-06-30 14:19:35 +0900

    프로젝트2.투두리스트-Create:투두추가하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -2,15 +2,55 @@ import './App.css';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import List from './components/List';
+import { useState, useRef } from 'react';
+
+// 다시 생성안해도 되므로 밖으로 컴포넌트 밖으로 뺌
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: 'React 공부하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: '빨래하기',
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: '노래 연습하기',
+    date: new Date().getTime(),
+  },
+];
+
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3); // 고유의 id값주기
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    }
+    // todos 배열에 newTodo추가
+    // todos.push(newTodo); 절대안됨.
+    // 상태변화 함수를 호출해서 수정해야됨 => state값을 리액트가 감지하고 컴포넌트를 정상적으로 리렌더링 할 수있음.
+    setTodos([newTodo, ...todos]); // 배열안에 기존 todos 가져오고, 두번째 인자에 새로운 데이터 넣기
+  }
+
   return (
     <div className="App">
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate} />
       <List />
     </div>
   )
 }
 
-export default App
+export default App;
--- section08/src/components/Editor.jsx
+++ section08/src/components/Editor.jsx
@@ -1,12 +1,40 @@
+import { useState, useRef } from "react";
 import "./Editor.css";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  // 인풋창 전용 상태는 여기서 관리
+  const [content, setContent] = useState('');
+  // 빈 값일 시 포커스
+  const contentRef = useRef();
+  
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  }
+
+  const onKeyDown = (e) => {
+    if(e.keyCode === 13) {
+      onSubmit();
+    }
+  }
+
+  const onSubmit = () => {
+    if (content.trim() === '') {
+      contentRef.current.focus();
+      return;
+    };
+    onCreate(content);
+    setContent('');
+  }
   return (
     <div className="Editor">
       <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
         placeholder="새로운 Todo.."
       />
-      <button>추가</button>
+      <button onClick={onSubmit}>추가</button>
     </div>
   )
 }
